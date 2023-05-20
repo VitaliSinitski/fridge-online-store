c4a185b285a4cdadb7ff92afc85d08e1e5de8bb5
vitali.sin777@gmail.com <vitali.sin777@gmail.com>
2023-05-20 20:55:20 +0300

    4.0 bugfix

---
+++ src/main/java/com/vitali/FridgeOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.vitali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeOnlineStoreApplication.class, args);
+	}
+
+}
